9651539b1e3a168504a1892017ebfd52c23ce312
joacoiannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2021-04-17 21:46:13 -0300

    initial commit

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -1,15 +1,19 @@
 package implementation;
 
+import cli.App;
 import interpreter.PrintScriptInterpreter;
 
 public class CustomImplementationFactory implements InterpreterFactory {
 
     @Override
     public PrintScriptInterpreter interpreter() {
-        // your PrintScript implementation should be returned here.
-        // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
 
-        // Dummy impl: return (src, version, emitter, handler) -> { };
+        return (src, version, emitter, handler) -> {
+            try {
+                App.run(src, version, emitter::print);
+            } catch (Exception e) {
+                handler.reportError(e.getMessage());
+            }
+        };
     }
 }
