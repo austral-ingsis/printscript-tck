3881f1f3c6db09c637f4141c89b252699c022305
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2021-05-07 18:35:25 -0300

    Implemented adapter for test suite

--- src/main/java/interpreter/ConcreteInterpreter.java
+++ src/main/java/interpreter/ConcreteInterpreter.java
@@ -10,16 +10,20 @@ public class ConcreteInterpreter implements PrintScriptInterpreter{
     public void execute(File src, String version, PrintEmitter emitter, ErrorHandler handler) {
         Parser parser = version.equals("1.0")? new PS10Parser() : new PS11Parser();
         String path = src.getPath();
-        DebugResult debugResult = parser.debug(path);
         String validationMessage = parser.validate(path);
 
         if(!validationMessage.equals("No errors encountered")){
             handler.reportError(validationMessage);
         } else {
-            List<String> prints = parser.execute(path);
-            for (String print : prints) {
-                emitter.print(print);
+            try{
+                List<String> prints = parser.execute(path);
+                for (String print : prints) {
+                    emitter.print(print);
+                }
+            } catch (RuntimeException e){
+                handler.reportError(e.getMessage());
             }
+
         }
     }
 }
