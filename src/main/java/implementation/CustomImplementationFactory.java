3287ec1964ebcd72006d349d1e1849e9f33c413f
tomasgp97 <tomas.gonzalezpalermo@ing.austral.edu.ar>
2021-05-07 06:09:40 -0300

    tck passed

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -1,6 +1,7 @@
 package implementation;
 
 import interpreter.PrintScriptInterpreter;
+import cli.Cli;
 
 public class CustomImplementationFactory implements InterpreterFactory {
 
@@ -8,8 +9,16 @@ public class CustomImplementationFactory implements InterpreterFactory {
     public PrintScriptInterpreter interpreter() {
         // your PrintScript implementation should be returned here.
         // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
 
-        // Dummy impl: return (src, version, emitter, handler) -> { };
+//        throw new NotImplementedException("Needs implementation"); // TODO: implement
+
+//         Dummy impl:
+        return (src, version, emitter, handler) -> {
+            try {
+                Cli.runCli(src, version, emitter::print);
+            } catch (Exception e) {
+                handler.reportError(e.getMessage());
+            }
+        };
     }
 }
