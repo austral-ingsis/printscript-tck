e15de871faca8706654a93ae3847d20b80b99baa
Tomas Fernandez <tfernandez@mulesoft.com>
2022-04-05 14:36:18 -0300

    removed 1.1 support in tck

--- src/main/java/interpreter/PrintScriptInterpreter.java
+++ src/main/java/interpreter/PrintScriptInterpreter.java
@@ -6,7 +6,7 @@ public interface PrintScriptInterpreter {
     /**
      * executes a PrintScript file handling its resulting messages and errors.
      * @param src Source file.
-     * @param version PrintScript version, 1.0 and 1.1 must be supported.
+     * @param version PrintScript version, 1.0
      * @param emitter interface where print statements must be called.
      * @param handler interface where all syntax and semantic error will be reported.
      */
