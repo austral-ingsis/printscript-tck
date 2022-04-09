adc0f68acf61b0c6b014b2c92d899782ef2b79a4
Tomás Bruno <tomas.bruno@ing.austral.edu.ar>
2022-04-09 19:18:34 -0300

    Adapted interpreter
    Co-authored-by: tomasberretta <tomas.berretta@ing.austral.edu.ar>

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -1,6 +1,16 @@
 package implementation;
 
+import ContentProvider.ContentProvider;
+import ContentProvider.FileContentProvider;
+import Interpreter.Interpreter;
+import Lexer.DefaultLexer;
+import Parser.DefaultParser;
+import interpreter.ErrorHandler;
+import interpreter.PrintEmitter;
 import interpreter.PrintScriptInterpreter;
+import org.austral.ingsis.printscript.parser.TokenIterator;
+
+import java.io.File;
 
 public class CustomImplementationFactory implements InterpreterFactory {
 
@@ -8,7 +18,21 @@ public class CustomImplementationFactory implements InterpreterFactory {
     public PrintScriptInterpreter interpreter() {
         // your PrintScript implementation should be returned here.
         // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
+        //throw new NotImplementedException("Needs implementation"); // TODO: implement
+
+        return new PrintScriptInterpreter() {
+            @Override
+            public void execute(File src, String version, PrintEmitter emitter, ErrorHandler handler) {
+                ContentProvider contentProvider = new FileContentProvider(src);
+                String toPrint = "";
+                try {
+                    toPrint = (new Interpreter()).run((new DefaultParser(TokenIterator.create(contentProvider.getContent(), (new DefaultLexer()).lex(contentProvider)))).parse()).read();
+                } catch (Throwable e) {
+                    handler.reportError(e.getMessage());
+                }
+                emitter.print(toPrint);
+            }
+        };
 
         // Dummy impl: return (src, version, emitter, handler) -> { };
     }
