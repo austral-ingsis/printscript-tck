2955cb596602b34ebc77d2d271f39a3bc55603e7
Mauro Kinderknecht <mauro.kinderknecht@ing.austral.edu.ar>
2022-04-25 02:29:33 -0300

    run tck

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -1,15 +1,56 @@
 package implementation;
 
+import interpreter.ErrorHandler;
+import interpreter.PrintEmitter;
 import interpreter.PrintScriptInterpreter;
+import impl.PrintScript;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.function.Consumer;
 
 public class CustomImplementationFactory implements InterpreterFactory {
 
     @Override
     public PrintScriptInterpreter interpreter() {
-        // your PrintScript implementation should be returned here.
-        // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
+        return (src, version, emitter, handler) -> {
+            String source = null;
+            try {
+                source = Files.readString(src.toPath(), StandardCharsets.US_ASCII);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            new PrintScript(new PrintEmitterAdapter(emitter), new ErrorHandlerAdapter(handler), version, false).interpret(source);
+        };
+    }
+}
 
-        // Dummy impl: return (src, version, emitter, handler) -> { };
+class PrintEmitterAdapter implements Consumer<String> {
+
+    PrintEmitter emitter;
+
+    public PrintEmitterAdapter(PrintEmitter emitter) {
+        this.emitter = emitter;
+    }
+
+    @Override
+    public void accept(String s) {
+        emitter.print(s);
+    }
+
+}
+
+class ErrorHandlerAdapter implements Consumer<String> {
+
+    ErrorHandler handler;
+
+    public ErrorHandlerAdapter(ErrorHandler handler) {
+        this.handler = handler;
+    }
+
+
+    @Override
+    public void accept(String s) {
+        handler.reportError(s);
     }
 }
