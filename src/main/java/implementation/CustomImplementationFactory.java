cb6e546695a10e67109caeeeb35f053047643efe
Tomás Bruno <tomas.bruno@ing.austral.edu.ar>
2022-05-03 00:33:40 -0300

    1.1 Support
    Co-authored-by: tomasberretta <tomas.berretta@ing.austral.edu.ar>

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -3,9 +3,15 @@ package implementation;
 import ContentProvider.ContentProvider;
 import ContentProvider.FileContentProvider;
 import Interpreter.Interpreter;
+import Interpreter.IInputProvider;
+import Interpreter.IPrintEmitter;
 import Lexer.DefaultLexer;
-import Parser.DefaultParser;
+import Lexer.TokenizerV1_0;
+import Lexer.TokenizerV1_1;
+import Parser.ProgramParserV1_0;
+import Parser.ProgramParserV1_1;
 import interpreter.ErrorHandler;
+import interpreter.InputProvider;
 import interpreter.PrintEmitter;
 import interpreter.PrintScriptInterpreter;
 import org.austral.ingsis.printscript.parser.TokenIterator;
@@ -20,17 +26,13 @@ public class CustomImplementationFactory implements InterpreterFactory {
         // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
         //throw new NotImplementedException("Needs implementation"); // TODO: implement
 
-        return new PrintScriptInterpreter() {
-            @Override
-            public void execute(File src, String version, PrintEmitter emitter, ErrorHandler handler) {
-                ContentProvider contentProvider = new FileContentProvider(src);
-                String toPrint = "";
-                try {
-                    toPrint = (new Interpreter()).run((new DefaultParser(TokenIterator.create(contentProvider.getContent(), (new DefaultLexer()).lex(contentProvider)))).parse()).read();
-                } catch (Throwable e) {
-                    handler.reportError(e.getMessage());
-                }
-                emitter.print(toPrint);
+        return (src, version, emitter, handler, provider) -> {
+            ContentProvider contentProvider = new FileContentProvider(src);
+            try {
+                if(version.equals("1.0")) emitter.print((new Interpreter()).run((new ProgramParserV1_0(TokenIterator.create(contentProvider.getContent(), (new DefaultLexer(new TokenizerV1_0())).lex(contentProvider)))).parse()).read());
+                else if(version.equals("1.1")) (new Interpreter()).run((new ProgramParserV1_1(TokenIterator.create(contentProvider.getContent(), (new DefaultLexer(new TokenizerV1_1())).lex(contentProvider)))).parse(), provider::input, emitter::print).read();
+            } catch (Throwable e) {
+                handler.reportError(e.getMessage());
             }
         };
 
