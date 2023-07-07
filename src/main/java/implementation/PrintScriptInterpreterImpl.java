614f0fc66b2556bf70a3dd9fb8698c0db8e18bec
matigayito <matigayito.mg@gmail.com>
2023-07-07 15:28:15 -0300

    error in string

--- src/main/java/implementation/PrintScriptInterpreterImpl.java
+++ src/main/java/implementation/PrintScriptInterpreterImpl.java
@@ -2,6 +2,9 @@ package implementation;
 
 import app.MyPrintScriptApp;
 import app.PrintScriptApp;
+import app.interpreter.PrintScriptInterpretStates;
+import app.printer.PrintScriptInterpretStatesPrinter;
+import app.printer.Printer;
 import interpreter.ErrorHandler;
 import interpreter.InputProvider;
 import interpreter.PrintEmitter;
@@ -17,6 +20,25 @@ public class PrintScriptInterpreterImpl implements PrintScriptInterpreterr {
             app.interpret(src);
         } else {
             System.out.println("Not implemented yet. Come back soon!");
+        }
+    }
+
+    class PrinterAdapter implements Printer<PrintScriptInterpretStates> {
+
+        PrintEmitter emitter;
+
+        public PrinterAdapter(PrintEmitter emitter) {
+            this.emitter = emitter;
+        }
+
+        @Override
+        public PrintScriptInterpretStates print(PrintScriptInterpretStates printScriptInterpretStates) {
+            PrintScriptInterpretStatesPrinter value = new PrintScriptInterpretStatesPrinter();
+            String toPrint = printScriptInterpretStates.component3().print().component1();
+            if (toPrint != null) {
+                emitter.print(toPrint);
+            }
+            return value.print(printScriptInterpretStates);
         }
     }
 }
