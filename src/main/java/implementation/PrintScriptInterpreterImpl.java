e4c5d972c2f1258ecb107eee565374176c10e7dd
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-13 15:43:13 -0300

    CustomErrorHandler

--- src/main/java/implementation/PrintScriptInterpreterImpl.java
+++ src/main/java/implementation/PrintScriptInterpreterImpl.java
@@ -1,11 +1,14 @@
 package implementation;
+import app.errorHandler.MyErrorHandler;
 import app.interpreter.Interpret;
 import app.interpreter.PrintScriptInterpetI;
+import app.interpreter.PrintScriptInterpretStates;
 import app.printer.interpret.PrintScriptInterpretStatesPrinter;
 import interpreter.ErrorHandler;
 import interpreter.InputProvider;
 import interpreter.PrintEmitter;
 import interpreter.PrintScriptInterpreterr;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.InputStream;
 
@@ -13,7 +16,7 @@ public class PrintScriptInterpreterImpl implements PrintScriptInterpreterr {
     @Override
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
         if (version.equals("1.0")) {
-            PrintScriptInterpetI printScriptInterpetI = new PrintScriptInterpetI("");
+            PrintScriptInterpetI printScriptInterpetI = new PrintScriptInterpetI("", new CustomErrorHandler(handler));
             PrintScriptInterpretStatesPrinter interpretStatesPrinter = new PrintScriptInterpretStatesPrinter(s -> {
                 if (s.endsWith(".0")) {
                     emitter.print(s.substring(0, s.length()-2));
@@ -26,6 +29,21 @@ public class PrintScriptInterpreterImpl implements PrintScriptInterpreterr {
             interpret.interpret(src);
         } else {
             System.out.println("Not implemented yet. Come back soon!");
+        }
+    }
+
+    class CustomErrorHandler implements app.errorHandler.ErrorHandler<PrintScriptInterpretStates> {
+
+        ErrorHandler handler;
+
+        public CustomErrorHandler(ErrorHandler handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public PrintScriptInterpretStates handle(@NotNull String s, PrintScriptInterpretStates printScriptInterpretStates) {
+            handler.reportError(s);
+            return new MyErrorHandler().handle(s, printScriptInterpretStates);
         }
     }
 }
