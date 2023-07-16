a27e1f5dd8dc8688ede3c659d98accd1de22c1a9
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-07-16 16:37:23 -0300

    Input works

--- src/main/java/implementation/PrintScriptInterpreterImpl.java
+++ src/main/java/implementation/PrintScriptInterpreterImpl.java
@@ -3,12 +3,14 @@ import app.errorHandler.MyErrorHandler;
 import app.interpreter.Interpret;
 import app.interpreter.PrintScriptInterpetI;
 import app.interpreter.PrintScriptInterpretStates;
+import app.literalInputter.LiteralInputter;
 import app.printer.interpret.PrintScriptInterpretStatesPrinter;
 import interpreter.ErrorHandler;
 import interpreter.InputProvider;
 import interpreter.PrintEmitter;
 import interpreter.PrintScriptInterpreterr;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.InputStream;
 
@@ -16,7 +18,7 @@ public class PrintScriptInterpreterImpl implements PrintScriptInterpreterr {
     @Override
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
         if (version.equals("1.0")) {
-            PrintScriptInterpetI printScriptInterpetI = new PrintScriptInterpetI("", new CustomErrorHandler(handler));
+            PrintScriptInterpetI printScriptInterpetI = new PrintScriptInterpetI("", new CustomErrorHandler(handler), new Inputter(provider));
             PrintScriptInterpretStatesPrinter interpretStatesPrinter = new PrintScriptInterpretStatesPrinter(s -> {
                 if (s.endsWith(".0")) {
                     emitter.print(s.substring(0, s.length()-2));
@@ -28,7 +30,7 @@ public class PrintScriptInterpreterImpl implements PrintScriptInterpreterr {
             Interpret interpret = new Interpret(interpretStatesPrinter, printScriptInterpetI);
             interpret.interpret(src);
         } else {
-            PrintScriptInterpetI printScriptInterpetI = new PrintScriptInterpetI("1.1", new CustomErrorHandler(handler));
+            PrintScriptInterpetI printScriptInterpetI = new PrintScriptInterpetI("1.1", new CustomErrorHandler(handler), new Inputter(provider));
             PrintScriptInterpretStatesPrinter interpretStatesPrinter = new PrintScriptInterpretStatesPrinter(s -> {
                 if (s.endsWith(".0")) {
                     emitter.print(s.substring(0, s.length()-2));
@@ -39,6 +41,21 @@ public class PrintScriptInterpreterImpl implements PrintScriptInterpreterr {
             });
             Interpret interpret = new Interpret(interpretStatesPrinter, printScriptInterpetI);
             interpret.interpret(src);
+        }
+    }
+
+    class Inputter implements LiteralInputter {
+
+        InputProvider provider;
+
+        public Inputter(InputProvider provider) {
+            this.provider = provider;
+        }
+
+        @Nullable
+        @Override
+        public String input() {
+            return provider.input("");
         }
     }
 
