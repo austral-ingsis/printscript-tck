c2f6b665d9bfbf47315dad23ea7ae245f6ab64b8
Juan Cruz Estevez <juanxestevez@gmail.com>
2023-07-16 13:14:04 -0300

    tck imp

---
+++ src/main/java/implementation/CustomInterpreter.java
@@ -0,0 +1,16 @@
+package implementation;
+
+import interpreter.ErrorHandler;
+import interpreter.InputProvider;
+import interpreter.PrintEmitter;
+import interpreter.PrintScriptInterpreter;
+
+import java.io.InputStream;
+
+public class CustomInterpreter implements PrintScriptInterpreter {
+
+    @Override
+    public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
+
+    }
+}
