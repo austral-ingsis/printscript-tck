b6f08c39fec96ecba138280cedf73b7c5f14d93e
Juan Cruz Estevez <juanxestevez@gmail.com>
2023-07-18 17:58:39 -0300

    tck imp

---
+++ src/main/java/implementation/InterpreterRuner.kt
@@ -0,0 +1,33 @@
+package implementation
+
+import consumer.*
+import interpreter.ErrorHandler
+import interpreter.InputProvider
+import interpreter.PrintEmitter
+
+class InterpreterRuner {
+    fun runConsumerInterpreter(consumer: ASTNodeConsumerInterpreter, emitter: PrintEmitter, handler: ErrorHandler,  provider: InputProvider) {
+        var result = consumer.consume()
+        while (result !is ConsumerResponseEnd) {
+            when (result) {
+                is ConsumerResponseSuccess -> {
+                    if (result.msg != null) {
+                        emitter.print(result.msg!!)
+                    }
+                }
+
+                is ConsumerResponseError -> {
+                    handler.reportError(result.error)
+                    return
+                }
+
+                is ConsumerResponseImput -> {
+                    emitter.print(result.msg)
+                    val input: String = provider.input(result.msg)
+                    consumer.getValue(input)
+                }
+            }
+            result = consumer.consume()
+        }
+    }
+}
