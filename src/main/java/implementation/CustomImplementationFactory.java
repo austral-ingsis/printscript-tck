184c95562a86aaeac8585dcc2cb8d0b1a5f9a61d
Juan Cruz Estevez <juanxestevez@gmail.com>
2023-07-18 17:11:48 -0300

    tck imp

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -8,8 +8,7 @@ public class CustomImplementationFactory implements InterpreterFactory {
     public PrintScriptInterpreter interpreter() {
         // your PrintScript implementation should be returned here.
         // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
-
+        return new CustomInterpreter();
         // Dummy impl: return (src, version, emitter, handler) -> { };
     }
 }
--- src/main/java/implementation/CustomInterpreter.java
+++ src/main/java/implementation/CustomInterpreter.java
@@ -1,16 +1,42 @@
 package implementation;
 
-import interpreter.ErrorHandler;
-import interpreter.InputProvider;
-import interpreter.PrintEmitter;
-import interpreter.PrintScriptInterpreter;
+import cli.Config;
+import consumer.*;
+import interpreter.*;
 
-import java.io.InputStream;
+import java.io.*;
 
 public class CustomInterpreter implements PrintScriptInterpreter {
 
     @Override
-    public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
+    public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider){
+        try {
+            ASTNodeConsumerInterpreter interpreter = initializeInterpreter(version, src);
+            runConsumerInterpreter(interpreter, emitter, handler, provider);
+        }catch (Throwable e) {
+            handler.reportError(e.getMessage());
+        }
 
+    }
+    public void runConsumerInterpreter(ASTNodeConsumerInterpreter consumer, PrintEmitter emitter, ErrorHandler handler, InputProvider provider){
+        ConsumerResponse result = consumer.consume();
+        while (!(result instanceof ConsumerResponseEnd)) {
+            if (result instanceof ConsumerResponseSuccess successResult) {
+                if (successResult.getMsg() != null) {
+                    emitter.print(successResult.getMsg());
+                }
+            } else if (result instanceof ConsumerResponseError errorResult) {
+                handler.reportError(errorResult.getError());
+                return;
+            } else if (result instanceof ConsumerResponseImput inputResult) {
+                emitter.print(inputResult.getMsg());
+                String input = provider.input(inputResult.getMsg()); // Assuming the method name is `Input`
+                consumer.getValue(input);
+            }
+            result = consumer.consume();
+        }
+    }
+    private ASTNodeConsumerInterpreter initializeInterpreter(String version, InputStream file){
+        return new Interpret(new Config().generateASTNproviderInputStream(version, file));
     }
 }
