07eccfcb80c86db68e4dc64bfaf062f5488b11f3
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-04-15 12:46:25 -0300

    solving some tests

--- src/main/java/implementation/Adapter.java
+++ src/main/java/implementation/Adapter.java
@@ -15,46 +15,32 @@ import java.util.List;
 class Adapter implements PrintScriptInterpreter {
     @Override
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
+        exec(src, version, emitter, handler, provider);
+    }
+
+    private void exec(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
         try {
-            //Todo : agregar version
-            List<AbstractSyntaxTree> ast = getAsts(src, provider, handler);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(src));
+            String line;
+            Lexer lexer = new Lexer(new ValueMapper(), new StrategyMapper());
             Interpreter interpreter = new Interpreter(getInputReaderType(provider));
-            List<String> response = Arrays.stream(interpreter.execute(ast).getString().split("\n")).toList();
-            response.forEach(emitter::print);
+
+            while ((line = reader.readLine()) != null) {
+                List<Token> tokens = lexer.execute(line);
+                List<AbstractSyntaxTree> ast = new Parser().execute(tokens);
+                String response = interpreter.execute(ast).getString();
+                if (!response.isBlank()) emitter.print(response);
+            }
+
+            reader.close();
         }
         catch (Exception e) {
             handler.reportError(e.getMessage());
         }
     }
+
     private InputReaderType getInputReaderType(InputProvider provider){
         return new AdaptInput(provider);
     }
 
-    private String getString(InputStream src) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(src));
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-            sb.append(line);
-            sb.append(System.lineSeparator());
-        }
-
-        reader.close();
-        return sb.toString();
-    }
-    private List <AbstractSyntaxTree> getAsts(InputStream src, InputProvider provider, ErrorHandler handler){
-        try {
-            String script = getString(src);
-            Lexer lexer = new Lexer(new ValueMapper(), new StrategyMapper());
-            List<Token> tokens = lexer.execute(script);
-            Parser parser = new Parser();
-            List<AbstractSyntaxTree> ast = parser.execute(tokens);
-            return ast;
-        }
-        catch (Exception e){
-            handler.reportError(e.getMessage());
-        }
-        return null;
-    }
 }
