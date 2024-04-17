eebed66cb001b463b227c44a65a199db3983b174
NachoEstevo <ignacio.estevo@ing.austral.edu.ar>
2024-04-17 19:01:04 -0300

    implementation packages

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -6,6 +6,7 @@ public class CustomImplementationFactory implements InterpreterFactory {
 
     @Override
     public PrintScriptInterpreter interpreter() {
+
         // your PrintScript implementation should be returned here.
         // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
         throw new NotImplementedException("Needs implementation"); // TODO: implement
