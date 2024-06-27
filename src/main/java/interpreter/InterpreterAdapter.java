0f20bbca1874a059f5be99696b8798ed04d62883
Franco B. <franco.barravecchia2002@gmail.com>
2024-06-27 10:33:13 -0300

    update: trying to use parser

--- src/main/java/interpreter/InterpreterAdapter.java
+++ src/main/java/interpreter/InterpreterAdapter.java
@@ -4,8 +4,20 @@ package interpreter;
 
 
 
+import ast.Scope;
+import kotlin.Pair;
+import kotlin.Result;
+import lexer.Context;
+import lexer.Lexer;
+import lexer.LexerImpl;
+import parser.MyParser;
+import parser.Parser;
+import stringReader.PartialStringReadingLexer;
+import token.TokenInfo;
+
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 
 public class InterpreterAdapter implements PrintScriptInterpreter {
@@ -14,8 +26,21 @@ public class InterpreterAdapter implements PrintScriptInterpreter {
         try {
             byte[] bytes = src.readNBytes(1046);
             //new PartialStringReadingLexer()
+            Lexer lexer = new LexerImpl();
+            PartialStringReadingLexer tokenizer =
+                    new PartialStringReadingLexer(lexer, "", new Context(0));
+
+            Parser parser = new MyParser();
+
             while (bytes.length > 0) {
                 String code = new String(bytes, StandardCharsets.UTF_8);
+                Pair<PartialStringReadingLexer, List<TokenInfo>> result = tokenizer.tokenizeString(code);
+                tokenizer = result.getFirst();
+
+                List<TokenInfo> tokens =result.getSecond();
+
+                //parser.
+                //Result<Scope> ast = parser.parseTokens(tokens);
 
 
                 bytes = src.readNBytes(1046);
