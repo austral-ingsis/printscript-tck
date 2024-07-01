8ff2ace1a8bedf9cb06fc132e48449c53b79eaff
Franco B. <franco.barravecchia2002@gmail.com>
2024-06-30 23:26:59 -0300

    update: adapt printscript

--- src/main/java/interpreter/InterpreterAdapter.java
+++ src/main/java/interpreter/InterpreterAdapter.java
@@ -1,23 +1,25 @@
 package interpreter;
 
-
-
-
-
-import ast.Scope;
+import adapters.ParserAdapterToJava;
+import adapters.ParserResult;
+import ast.AST;
+import factory.InterpreterFactoryImpl;
+import factory.LexerFactoryImpl;
+import interpreter.specializedInterpreter.CallInterpreter;
 import kotlin.Pair;
-import kotlin.Result;
 import lexer.Context;
 import lexer.Lexer;
 import lexer.LexerImpl;
-import parser.MyParser;
-import parser.Parser;
+import org.jetbrains.annotations.NotNull;
 import stringReader.PartialStringReadingLexer;
 import token.TokenInfo;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class InterpreterAdapter implements PrintScriptInterpreter {
@@ -25,12 +27,20 @@ public class InterpreterAdapter implements PrintScriptInterpreter {
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
         try {
             byte[] bytes = src.readNBytes(1046);
-            //new PartialStringReadingLexer()
-            Lexer lexer = new LexerImpl();
+            Lexer lexer = new LexerFactoryImpl(version).create();
+            CallInterpreter callInterpreter = CallInterpreter.INSTANCE;
+            new InterpreterFactoryImpl(version).create();
             PartialStringReadingLexer tokenizer =
                     new PartialStringReadingLexer(lexer, "", new Context(0));
 
-            Parser parser = new MyParser();
+
+            ParserAdapterToJava parser = new ParserAdapterToJava();
+            InputReader reader = new AdaptInput(provider);
+            Interpreter interpreter = new Interpreter(
+                    new Report(new ArrayList<>(), new ArrayList<>()),
+                    new HashMap<>(),
+                    reader
+            );
 
             while (bytes.length > 0) {
                 String code = new String(bytes, StandardCharsets.UTF_8);
@@ -38,15 +48,55 @@ public class InterpreterAdapter implements PrintScriptInterpreter {
                 tokenizer = result.getFirst();
 
                 List<TokenInfo> tokens =result.getSecond();
+                ParserResult parserResult = parser.parseTokens(tokens);
 
-                //parser.
-                //Result<Scope> ast = parser.parseTokens(tokens);
+                if (!parserResult.getSuccess()) {
+                    Throwable error = parserResult.getErr();
+                    if (error == null) error = new Throwable("unknown parse error");
+                    handler.reportError(error.getMessage());
+                    return;
+                }
+                AST ast = parserResult.getAst();
+                if (ast == null) {
+                    handler.reportError("unknown parse error");
+                    return;
+                }
 
+                interpreter = interpreter.interpret(ast);
+                if (!interpreter.getReport().getErrors().isEmpty()) {
+                    for (String error : interpreter.getReport().getErrors())
+                       handler.reportError(error);
+                    return;
+                }
+                if (!interpreter.getReport().getOutputs().isEmpty()) {
+                    for (String output : interpreter.getReport().getOutputs())
+                        emitter.print(output);
+                    interpreter = new Interpreter(
+                            new Report(new ArrayList<>(), new ArrayList<>()),
+                            interpreter.getVariables(),
+                            interpreter.getInputReader()
+                            );
+                }
 
                 bytes = src.readNBytes(1046);
             }
-        } catch (Exception err) {
-            handler.reportError(err.getMessage());
+
+            src.close();
+        } catch (Exception | Error exc) {
+            handler.reportError(exc.getMessage());
+        }
+    }
+
+    class AdaptInput implements InputReader{
+        private InputProvider provider;
+        public AdaptInput(InputProvider provider) {
+            this.provider = provider;
+        }
+
+        @NotNull
+        @Override
+        public InputReadResult readInput() {
+            return new InputReadResult(provider.input(""), this);
         }
     }
 }
