a8eccc025df8172deb10246a3b91e30cc782caa6
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-09-03 15:14:17 -0300

    fixing large file test

--- src/main/java/implementation/MyPrintScriptInterpreter.java
+++ src/main/java/implementation/MyPrintScriptInterpreter.java
@@ -9,24 +9,22 @@ import interpreter.PrintScriptInterpreter;
 import edu.Runner;
 
 import java.io.*;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class MyPrintScriptInterpreter implements PrintScriptInterpreter {
   @Override
   public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider){
-    Iterator<String> input = createIterator(src);
     openCollectors(emitter, handler, provider);
     try {
       Runner runner = new Runner(version);
-      runner.execute(input);
+      runner.execute(src);
     } catch (RuntimeException e) {
       System.err.println("Runtime error: " + e.getMessage());
       e.printStackTrace(System.err);
     } finally {
       closeCollectors(emitter, handler, provider);
     }
+
   }
 
   private void closeCollectors(PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
@@ -39,12 +37,5 @@ public class MyPrintScriptInterpreter implements PrintScriptInterpreter {
     System.setOut(printStream);
     PrintStream errorStream = new ErrorHandlerStream(System.err, handler);
     System.setErr(errorStream);
-  }
-
-  private Iterator<String> createIterator(InputStream src) {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(src));
-    List<String> lines = reader.lines().collect(Collectors.toList());
-    Iterator<String> iterator = lines.iterator();
-    return iterator;
   }
 }
--- src/main/java/implementation/MyPrintScriptLinter.java
+++ src/main/java/implementation/MyPrintScriptLinter.java
@@ -9,17 +9,13 @@ import interpreter.PrintScriptLinter;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class MyPrintScriptLinter implements PrintScriptLinter {
   @Override
   public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
-    Iterator<String> input = createIterator(src);
     Runner runner = new Runner(version);
     JsonObject c = createConfig(config);
-    Report report = runner.analyze(input, c);
+    Report report = runner.analyze(src, c);
     handleReport(report, handler);
   }
 
@@ -27,13 +23,6 @@ public class MyPrintScriptLinter implements PrintScriptLinter {
     for (String message: report.getMessages()) {
       handler.reportError(message);
     }
-  }
-
-  private Iterator<String> createIterator(InputStream src) {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(src));
-    List<String> lines = reader.lines().collect(Collectors.toList());
-    Iterator<String> iterator = lines.iterator();
-    return iterator;
   }
 
   private JsonObject createConfig(InputStream config) {
