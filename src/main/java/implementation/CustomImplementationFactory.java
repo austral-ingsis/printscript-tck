583a0e91b9712978c1f422c3d0ee9bcf104193ee
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-09-04 01:09:34 -0300

    Beginning to adapt

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -11,11 +11,8 @@ public class CustomImplementationFactory implements PrintScriptFactory {
 
     @Override
     public PrintScriptInterpreter interpreter() {
-        // your PrintScript implementation should be returned here.
-        // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
 
-        // Dummy impl: return (src, version, emitter, handler) -> { };
+      return new PrintScriptInterpreterImpl();
     }
 
     @Override
