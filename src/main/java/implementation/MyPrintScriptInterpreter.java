d269dd4fc638863c9f146e71ecd4110a6854af43
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-09-05 00:30:35 -0300

    First implementation of interpeter version of dependencies: 1.1.3-SNAPSHOT

--- src/main/java/implementation/MyPrintScriptInterpreter.java
+++ src/main/java/implementation/MyPrintScriptInterpreter.java
@@ -5,27 +5,21 @@ import interpreter.InputProvider;
 import interpreter.PrintEmitter;
 import interpreter.PrintScriptInterpreter;
 import org.Runner;
+import org.RunnerResult;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class MyPrintScriptInterpreter implements PrintScriptInterpreter {
     @Override
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
-        // Your implementation here
         Runner runner = new Runner(version);
-        // Triple<List<String>, List<String>, List<String>> result =
-        runner.execute(src.toString());
-        /*
-        for (String message : result.emitter()) {
-            emitter.print(message);
-        }
-        for (String message : result.handler()) {
-            handler.reportError(message);
-        }
-        for (String message : result.provider()) {
-            provider.input(message);
-        }
-         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(src));
+        String code = reader.lines().collect(Collectors.joining("\n"));
+        RunnerResult.Execute result = runner.execute(code);
+        result.getPrintList().forEach(emitter::print);
+        result.getErrorsList().forEach(handler::reportError);
     }
 }
