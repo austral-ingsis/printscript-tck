bb07c3edfdfb5aafe5d820a78c400ec95596d4a1
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-09-05 17:16:18 -0300

    Added MyPrintScriptLinter "same_line_for_else_brace"

--- src/main/java/implementation/MyPrintScriptLinter.java
+++ src/main/java/implementation/MyPrintScriptLinter.java
@@ -1,4 +1,18 @@
 package implementation;
 
-public class MyPrintScriptLinter {
+import interpreter.ErrorHandler;
+import interpreter.PrintScriptLinter;
+import kotlinx.serialization.json.JsonObject;
+import org.Runner;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyPrintScriptLinter implements PrintScriptLinter {
+    @Override
+    public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
+        Runner runner = new Runner(version);
+        JsonObject configJson = new Utils().getJsonFromStream(config);
+
+    }
 }
