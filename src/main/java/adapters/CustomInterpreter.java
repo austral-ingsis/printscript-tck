9dbe6010aaf350436d5988a64d7d1dced88c8baa
sonpipe0 <pipefc2003@gmail.com>
2024-09-06 02:27:36 -0300

    handle full bus

--- src/main/java/adapters/CustomInterpreter.java
+++ src/main/java/adapters/CustomInterpreter.java
@@ -17,7 +17,9 @@ import utils.InterpreterResult;
 import utils.ParsingResult;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 
 public class CustomInterpreter implements PrintScriptInterpreter {
@@ -25,29 +27,32 @@ public class CustomInterpreter implements PrintScriptInterpreter {
 
     @Override
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
-        if (!Objects.equals(version, "1.0")) {
-            handler.reportError("Invalid version");
-            throw new InterpreterException("Invalid version");
-        }
+        try {
+            if (!Objects.equals(version, "1.0")) {
+                handler.reportError("Invalid version");
+                throw new InterpreterException("Invalid version");
+            }
             BufferedReader reader = new BufferedReader(new InputStreamReader(src));
             Lexer lexer = new Lexer(reader, 0, new Position(1, 1));
             Iterator<Token> tokenIterator = lexer.tokenizeAll(lexer).iterator();
+
             Parser parser = new Parser(tokenIterator);
             Interpreter interpreter = new Interpreter();
 
-            while (tokenIterator.hasNext()) {
-                Sequence<ParsingResult> results = parser.parseExpressions();
-                Iterator<InterpreterResult> interpreterResults = interpreter.interpret(results).iterator();
-                while (interpreterResults.hasNext()) {
-                    InterpreterResult result = interpreterResults.next();
-                    if (result.hasException()) {
-                        handler.reportError(result.getException().getMessage());
-                    } else {
-                        emitter.print(result.getPrintln());
-                    }
-                }
 
-                results = null;
+            Sequence<ParsingResult> results = parser.parseExpressions();
+            Iterator<InterpreterResult> interpreterResults = interpreter.interpret(results).iterator();
+            while (interpreterResults.hasNext()) {
+                InterpreterResult result = interpreterResults.next();
+                if (result.hasException()) {
+                    handler.reportError(result.getException().getMessage());
+                } else {
+                    emitter.print(result.getPrintln());
+                }
             }
+        } catch ( Exception e) {
+            handler.reportError(e.getMessage());
+        }
+
     }
 }
