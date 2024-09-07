56fa9cc36ebfb3f27b3c74cd26c838d8aa856ffc
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-09-07 19:42:23 -0300

    implement linter adapter

--- src/main/java/interpreter/Interpreter.java
+++ src/main/java/interpreter/Interpreter.java
@@ -21,77 +21,77 @@ import java.util.Set;
 
 public class Interpreter implements PrintScriptInterpreter{
 
-  private final Lexer lexer = new PrintScriptLexer();
-  private final SyntaxAnalyzer syntaxAnalyzer = new SyntaxAnalyzerImpl();
-  private final SemanticAnalyzer semanticAnalyzer = createSemanticAnalyzer();
-  private final PrintBrokerObserver observer = new PrintBrokerObserver();
-  private final org.example.Interpreter interpreter = createInterpreter(observer);
+//  private final Lexer lexer = new PrintScriptLexer();
+//  private final SyntaxAnalyzer syntaxAnalyzer = new SyntaxAnalyzerImpl();
+//  private final SemanticAnalyzer semanticAnalyzer = createSemanticAnalyzer();
+//  private final PrintBrokerObserver observer = new PrintBrokerObserver();
+//  private final org.example.Interpreter interpreter = createInterpreter(observer);
 
 
 
   @Override
   public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
-    if(!version.equals("1.0")) return;
-
-    try{
-      BufferedReader reader = new BufferedReader(new InputStreamReader(src));
-      processLines(reader, emitter, handler);
-
-    } catch (IOException e){
-      handler.reportError("Error reading file");
-    }
-
-
+//    if(!version.equals("1.0")) return;
+//
+//    try{
+//      BufferedReader reader = new BufferedReader(new InputStreamReader(src));
+//      processLines(reader, emitter, handler);
+//
+//    } catch (IOException e){
+//      handler.reportError("Error reading file");
+//    }
+//
+//
 
   }
-
-  // Private
-  private SemanticAnalyzer createSemanticAnalyzer() {
-    final MapEnvironment env =
-        new MapEnvironment(
-          new HashMap<>(),
-          Set.of(
-            new Signature("println", List.of(DeclarationType.NUMBER)),
-            new Signature("println", List.of(DeclarationType.STRING))));
-
-    return new SemanticAnalyzerImpl(env);
-  }
-
-  private org.example.Interpreter createInterpreter(BrokerObserver<String> observer) {
-    InterpreterFactory factory = new InterpreterFactory();
-    factory.addObserver(observer);
-    return factory.create();
-  }
-
-  private void processLines(BufferedReader reader, PrintEmitter emitter, ErrorHandler handler) throws IOException{
-    List<Token> tokens = new ArrayList<>();
-    while(reader.ready()) {
-      Result lexerResult = lexer.lex(reader.readLine());
-
-      if (!lexerResult.isSuccessful()) {
-        handler.reportError(lexerResult.errorMessage());
-        return;
-      }
-      tokens.addAll(((LexerSuccess)lexerResult).getTokens());
-    }
-
-    SyntaxResult syntaxResult = syntaxAnalyzer.analyze(tokens);
-
-    if(!syntaxResult.isSuccessful()){
-      handler.reportError(syntaxResult.errorMessage());
-      return;
-    }
-
-    List<AstComponent> components = syntaxResult.getComponents();
-
-    Result semanticResult = semanticAnalyzer.analyze(components);
-    if(!semanticResult.isSuccessful()){
-      handler.reportError(semanticResult.errorMessage());
-      return;
-    }
-
-    interpreter.interpret(components);
-    emitter.print(observer.getPrintedOutput());
-    }
+//
+//  // Private
+//  private SemanticAnalyzer createSemanticAnalyzer() {
+//    final MapEnvironment env =
+//        new MapEnvironment(
+//          new HashMap<>(),
+//          Set.of(
+//            new Signature("println", List.of(DeclarationType.NUMBER)),
+//            new Signature("println", List.of(DeclarationType.STRING))));
+//
+//    return new SemanticAnalyzerImpl(env);
+//  }
+//
+//  private org.example.Interpreter createInterpreter(BrokerObserver<String> observer) {
+//    InterpreterFactory factory = new InterpreterFactory();
+//    factory.addObserver(observer);
+//    return factory.create();
+//  }
+//
+//  private void processLines(BufferedReader reader, PrintEmitter emitter, ErrorHandler handler) throws IOException{
+//    List<Token> tokens = new ArrayList<>();
+//    while(reader.ready()) {
+//      Result lexerResult = lexer.lex(reader.readLine());
+//
+//      if (!lexerResult.isSuccessful()) {
+//        handler.reportError(lexerResult.errorMessage());
+//        return;
+//      }
+//      tokens.addAll(((LexerSuccess)lexerResult).getTokens());
+//    }
+//
+//    SyntaxResult syntaxResult = syntaxAnalyzer.analyze(tokens);
+//
+//    if(!syntaxResult.isSuccessful()){
+//      handler.reportError(syntaxResult.errorMessage());
+//      return;
+//    }
+//
+//    List<AstComponent> components = syntaxResult.getComponents();
+//
+//    Result semanticResult = semanticAnalyzer.analyze(components);
+//    if(!semanticResult.isSuccessful()){
+//      handler.reportError(semanticResult.errorMessage());
+//      return;
+//    }
+//
+//    interpreter.interpret(components);
+//    emitter.print(observer.getPrintedOutput());
+//    }
 
 }
--- src/main/java/interpreter/Linter.java
+++ src/main/java/interpreter/Linter.java
@@ -1,10 +1,52 @@
 package interpreter;
 
-import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+import org.example.PrintScriptSca;
+import org.example.Result;
+import org.example.StaticCodeAnalyzer;
+
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
 
 public class Linter implements PrintScriptLinter{
   @Override
   public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
+    try {
+      if (!version.equals("1.0")) return;
+      InputStream adaptedConfig = adaptConfig(config);
+      StaticCodeAnalyzer linter = new PrintScriptSca(adaptedConfig);
+      Scanner scanner = new Scanner(src).useDelimiter("\n");
+      List<Result> results = linter.analyze(scanner);
+      for (Result result : results) {
+        if (result.isSuccessful()) continue;
+        handler.reportError(result.errorMessage());
+      }
+    } catch (IOException e) {
+        handler.reportError("Error reading file");
+    }
+  }
 
+  private String streamToString(InputStream stream) throws IOException {
+    StringBuilder build = new StringBuilder();
+    byte[] buf = new byte[1024];
+    int length;
+    try (InputStream is = stream) {
+      while ((length = is.read(buf)) != -1) {
+        build.append(new String(buf, 0, length));
+      }
+	}
+    return build.toString();
+  }
+
+  private InputStream adaptConfig(InputStream config) throws IOException {
+    String stringConfig = streamToString(config);
+    String adaptedStringConfig = stringConfig
+            .replace("identifier_format", "identifierFormat")
+            .replace("camel case", "camelCase")
+            .replace("snake case", "snakeCase")
+            .replace("\"mandatory-variable-or-literal-in-println\": true", "\"printlnExpressions\": false")
+            .replace("\"mandatory-variable-or-literal-in-println\": false", "\"printlnExpressions\": true");
+    return IOUtils.toInputStream(adaptedStringConfig);
   }
 }
