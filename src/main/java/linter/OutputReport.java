cb472f436ebf28cfae66c3cfd9e25892f847a557
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-08 00:00:41 -0300

    Implemented an OutputReport that will take care of the errors discovered by the linter.

---
+++ src/main/java/linter/OutputReport.java
@@ -0,0 +1,27 @@
+package linter;
+
+import runner.OutputResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutputReport implements OutputResult {
+  private List<String> errorCollector = new ArrayList<>();
+
+  @Override
+  public OutputResult saveResult(String s) {
+    if (!s.isEmpty()) {
+      errorCollector.add(s);
+    }
+    return this;
+  }
+
+  @Override
+  public String getResult() {
+    return "";
+  }
+
+  public List<String> getErrors() {
+    return errorCollector;
+  }
+}
