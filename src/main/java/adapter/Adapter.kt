a94c7fea01e97236c1d190e3564d555b5b4abfcc
maxim <maximiliano.maglio@ing.austral.edu.ar>
2024-09-11 19:32:25 -0300

    test with big input works

--- src/main/java/adapter/Adapter.kt
+++ src/main/java/adapter/Adapter.kt
@@ -7,6 +7,7 @@ import interpreter.InputAdapter
 import interpreter.OutputAdapterJava
 import parser.Parser
 import token.Token
+import token.TokenType
 import variable.VariableMap
 import java.io.InputStream
 
@@ -14,26 +15,40 @@ class Adapter {
     fun execute(src: InputStream, version: String, emitter: OutputAdapterJava, handler: ErrorHandler, provider: InputAdapter) {
         try {
             val versionController = LexerVersionController()
-
             val lexer = versionController.getLexer(version, src)
-            val tokens = mutableListOf<Token>()
+
+            var currentStatement = mutableListOf<Token>()
             var token = lexer.getNextToken()
+
             while (token != null) {
-                tokens.add(token)
+                currentStatement.add(token)
+
+                if (token.type == TokenType.SEMICOLON) {
+                    processStatement(currentStatement, version, emitter, provider)
+                    currentStatement.clear()
+                }
+
                 token = lexer.getNextToken()
             }
-            println(tokens)
-            val parser = Parser(tokens)
-            val ast = parser.generateAST()
-            val interpreter = InterpreterFactory(version , VariableMap(HashMap()), provider).buildInterpreter()
-            val interpretedList = interpreter.interpret(ast)
-            for (interpreted in interpretedList.second) {
-                emitter.print(interpreted)
+
+            // Procesar cualquier statement restante que no termine en ';'
+            if (currentStatement.isNotEmpty()) {
+                processStatement(currentStatement, version, emitter, provider)
             }
         } catch (e: Exception) {
             handler.reportError(e.message)
         } catch (e: Error) {
             handler.reportError(e.message)
+        }
+    }
+
+    private fun processStatement(statementTokens: List<Token>, version: String, emitter: OutputAdapterJava, provider: InputAdapter) {
+        val parser = Parser(statementTokens)
+        val ast = parser.generateAST()
+        val interpreter = InterpreterFactory(version , VariableMap(HashMap()), provider).buildInterpreter()
+        val interpretedList = interpreter.interpret(ast)
+        for (interpreted in interpretedList.second) {
+            emitter.print(interpreted)
         }
     }
 }
