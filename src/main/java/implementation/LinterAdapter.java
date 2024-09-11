6f761844a24a52076e8c8e781457e1bd3ac6513b
Bruno Iacovone <iacovonebru@gmail.com>
2024-09-11 15:27:36 -0300

    linter adapter

--- src/main/java/implementation/LinterAdapter.java
+++ src/main/java/implementation/LinterAdapter.java
@@ -8,27 +8,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinterAdapter implements PrintScriptLinter {
     @Override
     public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
-//        StringBuilder configuration = new StringBuilder();
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(config))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                configuration.append(line.replace("\r", " ")).append(System.lineSeparator());
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        try {
-//            Runner.lint(src, version, configuration.toString());
-//        } catch (Exception e) {
-//            handler.reportError(e.toString());
-//        }
+        StringBuilder configuration = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(config))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                configuration.append(line.replace("\r", " ")).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<String> errors = new ArrayList<>();
+        try {
+            errors = Runner.lint(src, version, configuration.toString());
+        } catch (Exception e) {
+            handler.reportError(e.toString());
+        }
+        for (String error : errors) {
+            handler.reportError(error);
+        }
     }
 }
