21d0a561623b477324f5741c1b4e0942b930f9e8
= <aalma@mail.austral.edu.ar>
2024-09-10 22:24:57 -0300

    start on formatter

---
+++ src/main/java/interpreter/Formatter.java
@@ -0,0 +1,20 @@
+package interpreter;
+
+import java.io.InputStream;
+import java.io.Writer;
+
+public class Formatter implements PrintScriptFormatter{
+    @Override
+    public void format(InputStream src, String version, InputStream config, Writer writer) {
+        try {
+            if (src == null || version == null || config == null || writer == null) {
+                throw new IllegalArgumentException();
+            }
+            if (!version.equals("1.0")) {
+                throw new IllegalArgumentException();
+            }
+        } catch (Exception e) {
+            throw  new RuntimeException();
+        }
+    }
+}
