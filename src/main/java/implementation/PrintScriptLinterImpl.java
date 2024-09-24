74210866e9368a7b4b1c43da7fc88993b69fe267
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-09-24 18:18:33 -0300

    changed build gradle to 1.1.1 & linter starting imp

--- src/main/java/implementation/PrintScriptLinterImpl.java
+++ src/main/java/implementation/PrintScriptLinterImpl.java
@@ -2,12 +2,29 @@ package implementation;
 
 import interpreter.ErrorHandler;
 import interpreter.PrintScriptLinter;
+import linters.StaticCodeAnalyzer;
+import org.example.commands.Analyze;
 
 import java.io.InputStream;
 
 public class PrintScriptLinterImpl implements PrintScriptLinter {
-    @Override
-    public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
-
+  @Override
+  public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
+    Analyze analyzer = new Analyze();
+    StaticCodeAnalyzer staticCodeAnalyzer = new StaticCodeAnalyzer(config, version);
+    //habría que pasar la config a la clase Configuration
+    try {
+      if (!version.equals("1.0") && !version.equals("1.1")) {
+        handler.reportError("Unsupported version: " + version);
+      } else {
+        try {
+          Object result = analyzer.run();
+        } catch (Exception e) {
+          handler.reportError("Error: " + e.getMessage());
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 }
