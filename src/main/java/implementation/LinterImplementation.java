a7c879ab05eb0a8ffb24ad4526059b9f223d652c
Facugarciar0805 <facugarciar@gmail.com>
2026-09-05 17:21:40 -0300

    anda el linter 1.0
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

---
+++ src/main/java/implementation/LinterImplementation.java
@@ -0,0 +1,35 @@
+package implementation;
+
+import interpreter.ErrorHandler;
+import interpreter.PrintScriptLinter;
+import linter.Linter;
+import linter.Warning;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class LinterImplementation implements PrintScriptLinter {
+
+    @Override
+    public void lint(InputStream src, String version, InputStream
+            config, ErrorHandler handler) {
+        try {
+            String sourceCode = new String(src.readAllBytes(),
+                    StandardCharsets.UTF_8);
+
+            TckLinterConfigAdapter adapter = new TckLinterConfigAdapter();
+            String internalConfigJson = adapter.adapt(config);
+            Linter linter = Linter.Companion.fromJson(internalConfigJson);
+            List<Warning> warningList = linter.analyse(sourceCode);
+            for(Warning warning: warningList){
+                handler.reportError(warning.getMessage());
+            }
+
+        } catch (IOException e) {
+            handler.reportError("Error al leer el archivo: " + e.
+                    getMessage());
+        }
+    }
+}
