e95629428d253c550436e02e72b7079431118b6e
Eliseo <ealanistoledo@mail.austral.edu.ar>
2026-09-14 00:02:17 -0300

    fix(tck): allocate memory reserve in interpreter adapter to reliably report OOM

--- src/main/java/implementation/PrintScriptInterpreterAdapter.java
+++ src/main/java/implementation/PrintScriptInterpreterAdapter.java
@@ -10,20 +10,34 @@ import printscript.runner.PrintScriptRunner;
 import java.io.InputStream;
 
 public class PrintScriptInterpreterAdapter implements PrintScriptInterpreter {
+    private static final int RESERVE_BYTES = 256 * 1024;
+
     @Override
     public void execute(InputStream src, String version, PrintEmitter emitter, ErrorHandler handler, InputProvider provider) {
-        PrintScriptRunner.INSTANCE.execute(
-            src,
-            version,
-            msg -> {
-                emitter.print(msg);
-                return Unit.INSTANCE;
-            },
-            provider::input,
-            err -> {
-                handler.reportError(err);
-                return Unit.INSTANCE;
-            }
-        );
+        byte[] reserve = new byte[RESERVE_BYTES];
+        try {
+            PrintScriptRunner.INSTANCE.execute(
+                src,
+                version,
+                msg -> {
+                    emitter.print(msg);
+                    return Unit.INSTANCE;
+                },
+                provider::input,
+                err -> {
+                    handler.reportError(err);
+                    return Unit.INSTANCE;
+                }
+            );
+        } catch (OutOfMemoryError e) {
+            reserve = null;
+            System.gc();
+            handler.reportError("Java heap space");
+        } catch (Throwable t) {
+            reserve = null;
+            handler.reportError(t.getMessage() != null ? t.getMessage() : t.toString());
+        } finally {
+            reserve = null;
+        }
     }
 }
